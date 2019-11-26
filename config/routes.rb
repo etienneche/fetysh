d9f04ce70101c911c52ad861c31fad5bca879544
Luana Almeida <luana.almeida.work@gmail.com>
2019-11-26 15:18:05 +0000

    Routes creation for reviews and reaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :articles
-
+  resources :articles do
+    resources :reviews
+    resources :reaction
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
