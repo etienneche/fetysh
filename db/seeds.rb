c5143332071e053f75ffd6b71bfcb223b222f2c6
etienne cherri <etienne@cherri.fr>
2019-11-26 20:17:44 +0000

    index page + the card effect for try

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 category = Category.create({name: "Orgasm"})
+article = Article.create({title: "blabla", content: "blabla"})
