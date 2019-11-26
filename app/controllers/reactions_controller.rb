1d5686ce4327a7c2df8860cd7caa2e71dd3e31c1
Luana Almeida <luana.almeida.work@gmail.com>
2019-11-26 14:30:20 +0000

    Reviews and reaction controller creation

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,2 +1,36 @@
 class ReactionsController < ApplicationController
+  before_action :set_reaction, only: [:create]
+
+  def create
+    @reaction = Reaction.new(reaction_params)
+    @reaction.user = current_user
+    @reaction.article = @article
+
+    if @reaction.save
+      redirect_to article_path(@article)
+    else
+      redirect_to article_path(@article)
+    end
+  end
+
+  def new
+    @reaction = Reaction.new
+  end
+
+  private
+
+  def reaction_params
+    params.require(:reaction).permit(:reaction)
+  end
+
+  def set_reaction
+    @reaction = Reaction.find(params[:id])
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
+
+
