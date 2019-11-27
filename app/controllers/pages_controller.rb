c3fde0cdc8e8a68183ce2045aba9b24754bdfc1d
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-27 16:06:04 +0000

    min adaaapts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:landing]
 
   def home
+    @articles = Article.all
+    @events = Event.all
   end
 
   def landing
