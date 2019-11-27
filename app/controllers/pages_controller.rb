eb39ef76ffc91b85ebd5407a295a94b4105b140e
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-27 15:51:47 +0000

    added login landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:landing]
+
   def home
+  end
+
+  def landing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#landing'
+  get '/home', to: 'pages#home', as: "home"
   resources :categories do
     resources :articles do
       resources :reviews
