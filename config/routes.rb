fadd720d67ed83780ff203e7ddf6dc2fefb66fb8
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-27 15:01:04 +0000

    seed fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
   resources :categories do
     resources :articles do
       resources :reviews
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
 puts 'Delete all articles_path'
+Reaction.destroy_all
 Article.destroy_all
+User.destroy_all
+
 puts 'Done'
+
+puts 'Create user'
+User.create!(
+  name:"Test",
+  password:"123456",
+  email:"test@test.com"
+)
+
 
 puts 'Create 5 categories'
   Category.create!(
@@ -31,8 +42,8 @@ puts 'Create 10 new articles'
   Article.create!(
     title: Faker::Restaurant.name,
     content: Faker::Lorem.paragraph,
-    user_id: 1,
-    category_id: 1,
+    user_id: User.first.id,
+    category_id: Category.first.id,
     img_url: 'https://source.unsplash.com/featured/?sex'
   )
 end
@@ -42,7 +53,7 @@ puts 'Done'
 puts 'Create 1 Reaction'
 
 Reaction.create!(
-  user_id: 1,
-  article_id: 21,
+  user_id: User.first.id,
+  article_id: Article.first.id,
   reaction: 'love'
 )
