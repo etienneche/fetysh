36b4dbb26cf9a217d7daa3ad8d65bc03d1556a6e
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-28 14:35:26 +0000

    added index for events

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,10 @@ import "bootstrap";
 
 require("trix")
 require("@rails/actiontext")
+
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
