1aa3a2c1e59d5b722f765a83f01a04e7360ec905
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-28 15:40:06 +0000

    added maps to index of events

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,4 +8,29 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
 
-initMapbox();
+
+const eventsindex = document.getElementsByClassName('.events-index')
+
+if (eventsindex){
+  initMapbox();
+}
+
+const usersedit = document.getElementsByClassName('.users-edit')
+
+if (useredit){
+  const tab1 = document.getElementById("link1")
+  const tab2 = document.getElementById("link2")
+  tab1.addEventListener('click', () => {
+    document.getElementById("try").style.display = "none"
+    document.getElementById("loved").style.display = "block"
+  })
+
+  tab2.addEventListener('click', () => {
+// evento de clicar Want to Try - id = test2
+document.getElementById("loved").style.display = "none"
+document.getElementById("try").style.display = "block"
+})
+}
+
+
+
