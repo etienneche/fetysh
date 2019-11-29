e49a32b14e76eae998792c873da06a3ae6b0d4d7
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-29 16:33:08 +0000

    fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,4 +32,29 @@ document.getElementById("try").style.display = "block"
 })
 }
 
+const articleshow = document.getElementsByClassName('.articles-show')
+
+if (articleshow) {
+  console.log("Hey there")
+  var button = document.getElementById("coracao")
+  count = 0;
+  button.onclick = function() {
+    count += 1;
+  }
+
+  var button2 = document.getElementById("marca")
+  count = 0;
+  button2.onclick = function() {
+    count += 1;
+  }
+
+  var button3 = document.getElementById("salva")
+  count = 0;
+  button2.onclick = function() {
+    count += 1;
+  }
+};
+
+
+
 
