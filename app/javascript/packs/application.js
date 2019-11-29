44bf525e438f21dec76f89c9b9dc1520b18c44ab
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-29 16:41:39 +0000

    js dirty fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,45 +15,45 @@ if (eventsindex){
   initMapbox();
 }
 
-const useredit = document.getElementsByClassName('.registrations-edit')
+// const useredit = document.getElementsByClassName('.registrations-edit')
 
-if (useredit){
-  const tab1 = document.getElementById("link1")
-  const tab2 = document.getElementById("link2")
-  tab1.addEventListener('click', () => {
-    document.getElementById("try").style.display = "none"
-    document.getElementById("loved").style.display = "block"
-  })
+// if (useredit){
+//   const tab1 = document.getElementById("link1")
+//   const tab2 = document.getElementById("link2")
+//   tab1.addEventListener('click', () => {
+//     document.getElementById("try").style.display = "none"
+//     document.getElementById("loved").style.display = "block"
+//   })
 
-  tab2.addEventListener('click', () => {
-// evento de clicar Want to Try - id = test2
-document.getElementById("loved").style.display = "none"
-document.getElementById("try").style.display = "block"
-})
-}
+//   tab2.addEventListener('click', () => {
+// // evento de clicar Want to Try - id = test2
+// document.getElementById("loved").style.display = "none"
+// document.getElementById("try").style.display = "block"
+// })
+// }
 
-const articleshow = document.getElementsByClassName('.articles-show')
+// const articleshow = document.getElementsByClassName('.articles-show')
 
-if (articleshow) {
-  console.log("Hey there")
-  var button = document.getElementById("coracao")
-  count = 0;
-  button.onclick = function() {
-    count += 1;
-  }
+// if (articleshow) {
+//   console.log("Hey there")
+//   var button = document.getElementById("coracao")
+//   count = 0;
+//   button.onclick = function() {
+//     count += 1;
+//   }
 
-  var button2 = document.getElementById("marca")
-  count = 0;
-  button2.onclick = function() {
-    count += 1;
-  }
+//   var button2 = document.getElementById("marca")
+//   count = 0;
+//   button2.onclick = function() {
+//     count += 1;
+//   }
 
-  var button3 = document.getElementById("salva")
-  count = 0;
-  button2.onclick = function() {
-    count += 1;
-  }
-};
+//   var button3 = document.getElementById("salva")
+//   count = 0;
+//   button2.onclick = function() {
+//     count += 1;
+//   }
+// };
 
 
 
