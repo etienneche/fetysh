facf413f4a4d78f05d390e5ebc007cb3fb0317e0
Martin Ditzel <ditzel.martin@gmail.com>
2019-12-03 16:00:08 +0000

    min style adapts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ Rating.destroy_all
 Order.destroy_all
 Event.destroy_all
 Article.destroy_all
+Category.destroy_all
 User.destroy_all
-
 
 puts 'Done'
 
@@ -18,40 +18,6 @@ User.create!(
   name: 'Scraper',
   password: '123456',
   email: 't@t.com'
-  )
-
-User.create!(
-  name: 'Kashima Berlin',
-  password: '123456',
-  email: 'c@d.com',
-  photo: 'https://www.kashima.de/fileadmin/template/images/logo.png'
-  )
-
-User.create!(
-  name: 'Pornceptual',
-  password: '123456',
-  email: 'a@a.com',
-  photo: 'https://www.residentadvisor.net/images/promoter/de-pornceptual.jpg',
-  description: 'Pornceptual presents pornography as queer, diverse and inclusive. We aim to prove that pornography can be respectful, intimate and artistic, while questioning usual pornographic labels. “Can art succeed where porn fails – to actually turn us on?”'
-  )
-
-User.create!(
-  name: 'Insomnia Berlin',
-  password: '123456',
-  email: 'b@b.com',
-  photo: 'https://www.insomnia-berlin.de/galpics/galleries/eventflyer/large/0000057589.jpg',
-  description: 'INSOMNIA is a club in which cosmopolitan freaks, urban night birds, polymorphic party people, creatives and travellers from all over the world celebrate together to the pulsating beat of the night.
-  INSOMNIA is much more than just a mixture of party, fetish and swinging clubs - it is a hedonistic parallel universe with parties that are sex-positive, full of casual intimacy and sensuality for all forms of love or relationships: whether vanilla, swinger, gender fluid, bizarre, fetish or BDSM.
-  INSOMNIA is a temple where you can experiment with non-traditional relationship structures, sexual experiences, self-exploration, and polysexuality. We offer a clean, safe sexual escapade infrastructure that is sexpositive & bodypositive, openminded, heteroflexible & clandestine - Newcomers are very welcome!
-  Get into your most eager role, your naughtiest costume, your breath-taking lingerie, your most daring outfits or your stylish fetish robe and make INSOMNIA what it is - with your intoxicating aura, your dazzling flair and your grandezza!
-  Be libidinous!'
-  )
-
-User.create!(
-  name: 'Factory Berlin',
-  password: '123456',
-  email: 'c@c.com',
-  photo: 'https://dr0wv9n0kx6h5.cloudfront.net/2754b559c5e6a1fd71093232df57dcf70462c5e7/content/03-blog/80-factory-berlin-a-new-place-for-us-to-call-home/01@2x.jpg'
   )
 
 puts 'Done'
@@ -180,11 +146,12 @@ topics.each do |topic|
       html_file = open(url).read
       html_doc = Nokogiri::HTML(html_file)
       content_all = html_doc.search('.article-rich-text.w-richtext').text
-      if content_all.index("Related Articles:‍").nil?
+      if content_all.index("Related Articles").nil?
         content = content_all
       else
-        content = content_all[0..content_all.index("Related Articles:‍") - 1]
+        content = content_all[0..(content_all.index("Related Articles") - 2)]
       end
+
       results << {
         category: html_doc.search('.current-topic').first.text.downcase,
         title: html_doc.search('.article-heading').text,
@@ -224,63 +191,63 @@ puts 'Scraping O.School is done'
 
 # SCRAPER EVENTBRITE -------------------------------------------------------------
 # SCRAPE LINKS TO ARTICLES
-puts 'start the scraper'
-results = []
+# puts 'start the scraper'
+# results = []
 
-locations = [
-  'united-kingdom',
-  'australia',
-  'united-states'
-]
+# locations = [
+#   'united-kingdom',
+#   'australia',
+#   'united-states'
+# ]
 
-locations.each do |location|
-  url = "https://www.eventbrite.com/d/#{location}/orgasm/"
-  html_file = open(url).read
-  html_doc = Nokogiri::HTML(html_file)
+# locations.each do |location|
+#   url = "https://www.eventbrite.com/d/#{location}/orgasm/"
+#   html_file = open(url).read
+#   html_doc = Nokogiri::HTML(html_file)
 
-  links = html_doc.search('.eds-media-card-content__action-link').map do |element|
-    element.attributes["href"].value
-  end
+#   links = html_doc.search('.eds-media-card-content__action-link').map do |element|
+#     element.attributes["href"].value
+#   end
 
-  # SCRAPE INDIVIDUAL ARTICLES
-  links.each do |link|
-    begin
-      puts "Putting #{link} into Nokogiri"
-      url = "#{link}"
-      html_file = open(url).read
-      html_doc = Nokogiri::HTML(html_file)
-      content_all = html_doc.search('.text-body-medium').text.gsub!("\n",'').gsub!("\t",'')
-      results << {
-        title: html_doc.search('.listing-hero-title').text,
-        address: html_doc.search('.listing-map-card-street-address.text-default').text.gsub!("\n",'').gsub!("\t",''),
-        organizer: html_doc.search('.js-d-scroll-to.listing-organizer-name.text-default').text.gsub!("\n",'').gsub!("\t",'').gsub!("by ",''),
-        description: content_all[0..content_all.index("atUse EventbritePlan") - 1],
-        photo: html_doc.search("picture").first.attributes["content"].value,
-        price_cents: html_doc.search('.js-display-price').text.gsub!("\n",'').gsub!("\t",'')[1..].to_i,
-        date: html_doc.search('.event-details__data').first.search("meta").first.attributes["content"].value
-      }
-    rescue => e
-      puts link
-      puts e
-    end
-  end
-end
+#   # SCRAPE INDIVIDUAL ARTICLES
+#   links.each do |link|
+#     begin
+#       puts "Putting #{link} into Nokogiri"
+#       url = "#{link}"
+#       html_file = open(url).read
+#       html_doc = Nokogiri::HTML(html_file)
+#       content_all = html_doc.search('.text-body-medium').text.gsub!("\n",'').gsub!("\t",'')
+#       results << {
+#         title: html_doc.search('.listing-hero-title').text,
+#         address: html_doc.search('.listing-map-card-street-address.text-default').text.gsub!("\n",'').gsub!("\t",''),
+#         organizer: html_doc.search('.js-d-scroll-to.listing-organizer-name.text-default').text.gsub!("\n",'').gsub!("\t",'').gsub!("by ",''),
+#         description: content_all[0..content_all.index("atUse EventbritePlan") - 1],
+#         photo: html_doc.search("picture").first.attributes["content"].value,
+#         price_cents: html_doc.search('.js-display-price').text.gsub!("\n",'').gsub!("\t",'')[1..].to_i,
+#         date: html_doc.search('.event-details__data').first.search("meta").first.attributes["content"].value
+#       }
+#     rescue => e
+#       puts link
+#       puts e
+#     end
+#   end
+# end
 
-puts 'Creating events from eventbrite scrape'
-results.each do |result|
-  Event.create!(
-    title: result[:title],
-    description: result[:description],
-    organizer: result[:organizer],
-    address: result[:address],
-    price_cents: result[:price_cents],
-    date: DateTime.parse(result[:date]),
-    user_id: User.find_by(name: 'Scraper').id,
-    category_id: Category.find_by(name: 'sex ed').id,
-    photo: result[:photo]
-    )
-end
-puts 'Scraping O.School is done'
+# puts 'Creating events from eventbrite scrape'
+# results.each do |result|
+#   Event.create!(
+#     title: result[:title],
+#     description: result[:description],
+#     organizer: result[:organizer],
+#     address: result[:address],
+#     price_cents: result[:price_cents],
+#     date: DateTime.parse(result[:date]),
+#     user_id: User.find_by(name: 'Scraper').id,
+#     category_id: Category.find_by(name: 'sex ed').id,
+#     photo: result[:photo]
+#     )
+# end
+# puts 'Scraping O.School is done'
 
 
 # ------------------- EVENTS SEED ----------------------------------------------
@@ -292,7 +259,8 @@ Event.create!(
   date: DateTime.parse("06/08/2020 20:00"),
   description: "In the spirit of recent political events, we invite you to celebrate unity and all that brings us together. With the rise of far-right groups in the European Parliament, fighting to stop the legitimization of hate-fueled narratives is now more than ever necessary. For the next edition of Pornceptual, we will use porn to expose and challenge anti-democratic ideologies and their continued existence. Naked bodies have no frontiers.
   Our political awareness efforts aren’t about praising EU as an institution, but to honor the idea of a united continent. We will move forward to propose a new form of UNION that will create a global identity, in opposition to nationalism and borders. At PORN UNION, you’ll be part of a symbolic fight against everything that separates us. We believe in the exploration of one’s uniqueness and giving an opportunity to feel what unification has to offer. We encourage you to present your differences and experience others shamelessly. Find the same kinks and discover new ones. Come, open your borders and show that we are stronger than ever!",
-  user_id: User.find_by(name: "Pornceptual").id,
+  organizer: "Pornceptual",
+  user_id: User.find_by(name: "Scraper").id,
   category_id: Category.find_by(name: "sex positive").id,
   photo: 'https://images.unsplash.com/photo-1517263904808-5dc91e3e7044?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
   price: rand(40..200)
@@ -301,11 +269,12 @@ Event.create!(
 Event.create!(
   title: "1st Sex-positive Ball",
   address: 'Insomnia Berlin',
-  date: DateTime.parse("30/11/2019 20:00"),
+  date: DateTime.parse("30/01/2019 20:00"),
   description: "The European Sex-positive Community launches its first sex-positive Ball this November at the Insomnia Berlin!
   This is more than just an evening but a whole weekend dedicated to sex-positivity with talks, workshops, a kinky auction and, as grand finale, the costume ball !!
   And we are all about party-cipation! The event is a fundraiser for the s+ community europe project (more infos sex-positive.com).",
-  user_id: User.find_by(name: "Insomnia Berlin").id,
+  user_id: User.find_by(name: "Scraper").id,
+  organizer: "Insomnia Berlin",
   category_id: Category.find_by(name: "sex positive").id,
   photo: 'https://www.insomnia-berlin.de/galpics/galleries/eventflyer/large/0000057588.jpg',
   price: rand(40..200)
@@ -320,7 +289,8 @@ Event.create!(
   The naughty Thursday at INSOMNIA! We start up early in the afternoon when the men-craving ladies from the Club of nymphomaniacs pursue their insatiable instincts and preferably get satisfied by a pack of men.
   Later in the evening they are joined by adventurous couples, who experiment with all sorts of fantasies. One may give away his wife blindfolded, the next wants a group insemination, the active lady showing off her cuckold, etc.
   A hot idea being used by a horny couple for their dirty fantasy ... isn't it?",
-  user_id: User.find_by(name: "Insomnia Berlin").id,
+  user_id: User.find_by(name: "Scraper").id,
+  organizer: 'Insomnia Berlin',
   category_id: Category.find_by(name: "fetish").id,
   photo: 'https://www.insomnia-berlin.de/galpics/galleries/eventflyer/large/0000042860.jpg',
   price: rand(40..200)
@@ -329,9 +299,10 @@ Event.create!(
 Event.create!(
   title: "Tantra Massage Training",
   address: 'Kashima Berlin',
+  organizer: 'Kashima Berlin',
   date: DateTime.parse("30/03/2020 10:00"),
   description: "During the Tantra Massage training you learn how to express respect and appreciation towards a person as a sexual being. For this you need to be willing to question your own behavioural patterns. We will discuss in great detail what Tantra means and how we can bring to life the tantric teachings in a massage. You will learn about the study of auras. With intensive exercises we increase our physical awareness.",
-  user_id: User.first.id,
+  user_id: User.find_by(name: "Scraper").id,
   category_id: Category.find_by(name: "tantra").id,
   photo: 'https://images.unsplash.com/photo-1545183322-6da710083410?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
   price: rand(40..200)
@@ -346,7 +317,7 @@ Event.create!(
   The Squirtinator shows the ladies and their partners where the G spot lies and which sparkling pleasures one can trigger with it.
   Our club of the nymphomaniacs is always haunted by such desires, which is why the most naughty of the ladies are especially looking forward to this date. They are craving for squirty games.",
   user_id: User.find_by(name: "Scraper").id,
-  user_id: User.find_by(name: "Insomnia Berlin").id,
+  organizer: 'Insomnia Berlin',
   category_id: Category.find_by(name: "fetish").id,
   photo: 'https://www.insomnia-berlin.de/galpics/galleries/eventflyer/large/0000054573.jpg',
   price: rand(40..200)
@@ -369,6 +340,7 @@ Event.create!(
 Event.create!(
   title: "Orgasmic Flowing - Sacred Tantra Workshop Berlin",
   address: 'Diamond Lotus Berlin',
+  organizer: 'Diamond Lotus Berlin',
   date: DateTime.parse("30/01/2020 18:00"),
   description: "How much do you embrace your ORGASMIC being?
   How much are you breathing in LIFE and BLISS into all your cells? Do you reall say YES to your joy?
@@ -376,7 +348,7 @@ Event.create!(
   This evening is about feeling myself, freeing my orgasmic being, breathing into my soul, honouring my YES and embodying divine pleasure ~ coming home to the blissful and orgasmic being that we are!
   We will share a unique and specific orgasmic meditation with you that is also part of the Mastery Retreat Free your Soul!
   There is no nudity or sexual activities in this Tantra workshop. The workshop is suitable for singles, couples, multi orgasmic beings and for people that have never experienced any orgasm.",
-  user_id: User.first.id,
+  user_id: User.find_by(name: "Scraper").id,
   category_id: Category.find_by(name: "tantra").id,
   photo: 'https://scontent.flis8-2.fna.fbcdn.net/v/t31.0-8/14362665_639659282875092_7117318554322478844_o.jpg?_nc_cat=100&_nc_ohc=uGYNN07uDZcAQk0_EJGrOMcqJTrXDcM2VO7sxEA5vzboMkHGysM5GURtg&_nc_ht=scontent.flis8-2.fna&oh=375b3ca497287c69fdafb4c4b01adde4&oe=5E427DE6',
   price: rand(40..200)
