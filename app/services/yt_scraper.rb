6808339af22431c5899e050e387acb232f4c3088
Martin Ditzel <ditzel.martin@gmail.com>
2019-12-04 15:55:25 +0000

    small fix

---
+++ app/services/yt_scraper.rb
@@ -0,0 +1,13 @@
+require 'byebug'
+require 'nokogiri'
+require 'open-uri'
+
+puts "Starting to scrape YT"
+url = "https://www.youtube.com/channel/UCIRoo9Lsz1DOXRIvEo-UARQ/videos"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+byebug
+links = html_doc.search('.Blog-header-content-link').map do |element|
+  element.attributes["href"].value
+end
